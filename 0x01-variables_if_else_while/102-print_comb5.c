27368381a6bc4c8aef99dea521ff665b742991d4
Miss-Rey <audreychelangat2003@gmail.com>
2023-12-18 19:52:36 +0300

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,32 +5,25 @@
  * Return: 0 for sucess
  */
 
-int main(void)
-{
-	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		for (j = '0'; j <= '9'; j++)
-		{
-			if (i < j)
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
-				{
-					continue;
-				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-
-	putchar('\n');
-
-	return (0);
+int main(void) {
+    int i = 0;
+    while (i < 100) {
+        int j = i + 1; // Ensures unique combinations
+        while (j < 100) {
+            putchar((i / 10) + '0');
+            putchar((i % 10) + '0');
+            putchar(' ');
+            putchar((j / 10) + '0');
+            putchar((j % 10) + '0');
+            
+            if (i != 98 || j != 99) { // Avoids printing comma at the end
+                putchar(',');
+                putchar(' ');
+            }
+            j++;
+        }
+        i++;
+    }
+    return 0;
 }
