a0100f77ddfd94bdd2083cdd210875ef9b2ccebc
Miss-Rey <audreychelangat2003@gmail.com>
2023-12-18 18:47:15 +0300

    Name and Hello,puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle , followed by a new line."
- * Return: Always 0
-*/
-int main(void)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
+int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
     return (0);
 }
+
