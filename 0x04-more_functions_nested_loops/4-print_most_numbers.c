b36284d15c34446f828de80eac0f2209341e87e8
Miss-Rey <audreychelangat2003@gmail.com>
2023-12-21 15:13:38 +0300

    print-most-numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,23 +2,22 @@
 
 /**
  * print_most_numbers - prints numbers from 0 to 9
- *  excluding 2 and 4
+ * except 2 and 4
  */
 
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for(i = 0; i < 10; i++)
-    {
-        if(i == 2 || i == 4)
-        {
-            continue;
-        }
-         
-        _putchar(i + '0');
-                
-    }
-    
-    _putchar("\n");
+	for (i = 0; i < 10; i++)
+	{
+		if (i == 2 || i == 4)
+		{
+			continue;
+		}
+
+		_putchar(i + '0');
+	}
+
+	_putchar('\n');
 }
